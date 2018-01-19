f3b89d67f57e30aa935de39a4c77196b32c46c6b
Marco Nembrini <marco@3ap.ch>
2018-01-19 15:21:21 +0100

    Initial commit with project structure and gradle build script

---
+++ REAMDE.md
@@ -0,0 +1,4 @@
+# A Harvest Java Client
+
+harvest-client is a Java client for the Harvest REST API (https://help.getharvest.com/api-v2/)
+
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void testSample() {
+
+        System.out.println("Output from test");
+        assertEquals(1,1);
+
+    }
+
+}
