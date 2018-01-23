6951884bdda25c6fccac677d8427dedc81f1a09d
Marco Nembrini <marco@3ap.ch>
2018-01-23 16:15:11 +0100

    add logging with SLF4j and log4j at runtime (client code can choose something else)

--- src/main/java/impl/UsersApiImpl.java
+++ src/main/java/impl/UsersApiImpl.java
@@ -3,6 +3,8 @@ package impl;
 import api.UsersApi;
 import domain.User;
 import domain.Users;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import retrofit2.Call;
 import retrofit2.Response;
 import service.UserService;
@@ -12,6 +14,8 @@ import java.util.Collections;
 import java.util.List;
 
 public class UsersApiImpl implements UsersApi {
+
+    private static final Logger log = LoggerFactory.getLogger(UsersApiImpl.class);
     private final UserService service;
 
     public UsersApiImpl(UserService userService) {
@@ -26,7 +30,7 @@ public class UsersApiImpl implements UsersApi {
             return response.body().getUsers();
 
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("", e);
         }
         return Collections.emptyList();
     }
