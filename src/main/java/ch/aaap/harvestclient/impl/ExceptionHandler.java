68c07c5b17ab3b948cf9b1b21fd29d74ead11a9b
Marco Nembrini <marco@3ap.ch>
2018-02-02 16:04:38 +0100

    add automatic retry by default for server Rate-Limiting

--- src/main/java/ch/aaap/harvestclient/impl/ExceptionHandler.java
+++ src/main/java/ch/aaap/harvestclient/impl/ExceptionHandler.java
@@ -1,11 +1,18 @@
 package ch.aaap.harvestclient.impl;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ch.aaap.harvestclient.exception.*;
+import okhttp3.Headers;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Response;
@@ -24,7 +31,7 @@ public class ExceptionHandler {
                 return response.body();
             } else {
                 int code = response.code();
-                log.debug("Failure -> {}", code);
+                log.warn("Failure -> {}", code);
                 ResponseBody errorBody = response.errorBody();
                 switch (code) {
                     case 401:
@@ -36,7 +43,7 @@ public class ExceptionHandler {
                     case 422:
                         throw new RequestProcessingException(errorBody);
                     case 429:
-                        throw new RateLimitedException(errorBody);
+                        return retryLater(call, response);
                     case 500:
                         throw new ServerErrorException(errorBody, 500);
                     default:
@@ -48,4 +55,62 @@ public class ExceptionHandler {
             throw new HarvestRuntimeException(e);
         }
     }
+
+    private static <T> T retryLater(Call<T> failedCall, Response<T> failedResponse) {
+
+        int maxWait = 30;
+        int wait = 0;
+        if (failedResponse.code() == 429) {
+            int secondsToWait = parseRetryAfter(failedResponse);
+            wait = Math.min(secondsToWait, maxWait);
+        }
+
+        try {
+            log.warn("Waiting {} seconds to retry call {}", wait, failedCall.request());
+            Thread.sleep(TimeUnit.SECONDS.toMillis(wait));
+        } catch (InterruptedException e) {
+            throw new HarvestRuntimeException(e);
+        }
+
+        Call<T> call = failedCall.clone();
+        // TODO a server returning constant 429 will put us in a loop
+        return callOrThrow(call);
+    }
+
+    /**
+     * See https://tools.ietf.org/html/rfc2616#section-14.37 for the format
+     * 
+     * @param <T>
+     * @param failedResponse
+     * @return the number of seconds to wait until we can retry
+     */
+    private static <T> int parseRetryAfter(Response<T> failedResponse) {
+
+        int wait = 1;
+        // extract retry-after headers
+        Headers headers = failedResponse.headers();
+        String header = headers.get("Retry-After");
+
+        if (header == null) {
+            // we don't understand the header
+            log.error("Missing Retry-After Header for {}", failedResponse);
+            return wait;
+        }
+        try {
+            // is it an int?
+            return Integer.parseInt(header);
+        } catch (NumberFormatException e) {
+            try {
+                ZonedDateTime date = ZonedDateTime.parse(header, DateTimeFormatter.RFC_1123_DATE_TIME);
+                // we don't care for values bigger than Integer.MAX_VALUE
+                log.debug("Retry-After is {} ", date);
+                return (int) Duration.between(Instant.now(), date).getSeconds();
+
+            } catch (DateTimeParseException e1) {
+                log.error("Invalid Retry-After Header for {}", failedResponse);
+                return wait;
+            }
+        }
+    }
+
 }
