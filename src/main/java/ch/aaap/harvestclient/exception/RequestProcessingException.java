efaab1ea94122644fbd9fc94becdc50621de1b08
Marco Nembrini <marco@3ap.ch>
2018-02-16 14:58:16 +0100

    improve http exception logging

--- src/main/java/ch/aaap/harvestclient/exception/RequestProcessingException.java
+++ src/main/java/ch/aaap/harvestclient/exception/RequestProcessingException.java
@@ -1,8 +1,5 @@
 package ch.aaap.harvestclient.exception;
 
-import com.google.gson.Gson;
-
-import ch.aaap.harvestclient.core.gson.GsonConfiguration;
 import okhttp3.ResponseBody;
 
 public class RequestProcessingException extends HarvestHttpException {
@@ -10,17 +7,7 @@ public class RequestProcessingException extends HarvestHttpException {
     private static final long serialVersionUID = -3464804468055061178L;
 
     public RequestProcessingException(ResponseBody responseBody) {
-        super(responseBody, 422, initMessage(responseBody));
-
-    }
-
-    public static String initMessage(ResponseBody responseBody) {
-        Gson gson = GsonConfiguration.getConfiguration();
-
-        RequestProcessingErrorMessage parsedMessage = gson.fromJson(responseBody.charStream(),
-                RequestProcessingErrorMessage.class);
-
-        return parsedMessage.getMessage();
+        super(responseBody, 422);
     }
 
 }
