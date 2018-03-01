a705335359e4e513e074070e34cc101d467da915
Marco Nembrini <marco@3ap.ch>
2018-03-01 14:02:51 +0100

    fix expense Category DTO

--- src/main/java/ch/aaap/harvestclient/domain/reference/dto/ExpenseCategoryReferenceDto.java
+++ src/main/java/ch/aaap/harvestclient/domain/reference/dto/ExpenseCategoryReferenceDto.java
@@ -1,9 +1,9 @@
 package ch.aaap.harvestclient.domain.reference.dto;
 
-import ch.aaap.harvestclient.domain.Expense;
+import ch.aaap.harvestclient.domain.ExpenseCategory;
 import ch.aaap.harvestclient.domain.reference.Reference;
 
-public class ExpenseCategoryReferenceDto extends BaseReferenceDto implements Reference<Expense> {
+public class ExpenseCategoryReferenceDto extends BaseReferenceDto implements Reference<ExpenseCategory> {
 
     public ExpenseCategoryReferenceDto() {
     }
