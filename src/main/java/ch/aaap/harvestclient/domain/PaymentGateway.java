d6ea47063ceab286bca4b3334b036127895f64b3
Marco Nembrini <marco@3ap.ch>
2018-03-05 14:05:31 +0100

    add fields for objects that don't have their own API endpoint

--- src/main/java/ch/aaap/harvestclient/domain/PaymentGateway.java
+++ src/main/java/ch/aaap/harvestclient/domain/PaymentGateway.java
@@ -1,10 +1,21 @@
 package ch.aaap.harvestclient.domain;
 
+import javax.annotation.Nullable;
+
 import org.immutables.gson.Gson;
 import org.immutables.value.Value;
 
+import ch.aaap.harvestclient.domain.reference.Reference;
+
 @Gson.TypeAdapters(fieldNamingStrategy = true)
 @Value.Immutable
-public interface PaymentGateway {
+public interface PaymentGateway extends Reference<PaymentGateway> {
+
+    @Override
+    @Nullable
+    Long getId();
+
+    @Nullable
+    String getName();
 
 }
