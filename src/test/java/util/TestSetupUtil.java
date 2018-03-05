a65f8ecbeba2d4f23894da44837b4ab6981d63f1
Marco Nembrini <marco@3ap.ch>
2018-03-05 12:12:28 +0100

    change how environment variable provide Harvest account tokens
    See TestSetupUtil.loadConfiguration() for details

--- src/test/java/util/TestSetupUtil.java
+++ src/test/java/util/TestSetupUtil.java
@@ -9,11 +9,14 @@ import org.apache.logging.log4j.core.LoggerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigValueFactory;
 
 import ch.aaap.harvestclient.core.Harvest;
 import ch.aaap.harvestclient.domain.ImmutableUser;
 import ch.aaap.harvestclient.domain.User;
+import ch.aaap.harvestclient.exception.HarvestRuntimeException;
 
 public class TestSetupUtil {
 
@@ -21,11 +24,11 @@ public class TestSetupUtil {
     private static final Random random = new Random();
 
     private static class LazyHolder {
-        static final Harvest adminAccess = new Harvest(ConfigFactory.load("admin1"));
+        static final Harvest adminAccess = new Harvest(loadConfiguration("admin1"));
     }
 
     private static class LazyHolder2 {
-        static final Harvest anotherAdminAccess = new Harvest(ConfigFactory.load("admin2"));
+        static final Harvest anotherAdminAccess = new Harvest(loadConfiguration("admin2"));
     }
 
     public static Harvest getAdminAccess() {
@@ -34,6 +37,36 @@ public class TestSetupUtil {
 
     public static Harvest getAnotherAdminAccess() {
         return LazyHolder2.anotherAdminAccess;
+    }
+
+    /**
+     * Try to load a local conf file first, and override values with environment
+     * variables if they are set.
+     * 
+     * @param name
+     *            the name of the configuration to load. Allowed characters are the
+     *            same as for environment variables (A-Z, a-z, 0-9, _)
+     * @return the configuration with env overrides applied
+     */
+    private static Config loadConfiguration(String name) {
+        // check for allowed characters
+        if (name.matches("^[A-Za-z0-9_]]")) {
+            throw new HarvestRuntimeException("Configuration name " + name + " contains invalid characters");
+        }
+
+        Config config = ConfigFactory.load(name);
+
+        String harvestAccountId = System.getenv("HARVEST_ACCOUNT_ID_" + name.toUpperCase());
+        String harvestAuthToken = System.getenv("HARVEST_AUTH_TOKEN_" + name.toUpperCase());
+        if (harvestAccountId != null) {
+            config = config.withValue("harvest.auth.accountId", ConfigValueFactory.fromAnyRef(harvestAccountId));
+        }
+        if (harvestAuthToken != null) {
+            config = config.withValue("harvest.auth.token", ConfigValueFactory.fromAnyRef(harvestAuthToken));
+        }
+
+        return config;
+
     }
 
     public static User getRandomUserCreationInfo() {
