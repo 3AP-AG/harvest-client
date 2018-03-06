0900dcd916fbe667be3d38e3e620e2b93a4c5fec
Marco Nembrini <marco@3ap.ch>
2018-03-06 11:50:23 +0100

    filter tests for Project.list() byClient and byActive

--- src/test/java/ch/aaap/harvestclient/impl/project/ProjectsApiListTest.java
+++ src/test/java/ch/aaap/harvestclient/impl/project/ProjectsApiListTest.java
@@ -1,22 +1,41 @@
 package ch.aaap.harvestclient.impl.project;
 
+import java.time.Instant;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import ch.aaap.harvestclient.HarvestTest;
 import ch.aaap.harvestclient.api.ProjectsApi;
 import ch.aaap.harvestclient.api.filter.ProjectFilter;
+import ch.aaap.harvestclient.core.Harvest;
+import ch.aaap.harvestclient.domain.Client;
+import ch.aaap.harvestclient.domain.ImmutableProject;
 import ch.aaap.harvestclient.domain.Project;
 import ch.aaap.harvestclient.domain.pagination.Pagination;
+import ch.aaap.harvestclient.domain.reference.Reference;
+import util.ExistingData;
 import util.TestSetupUtil;
 
 @HarvestTest
 class ProjectsApiListTest {
 
-    private static final ProjectsApi projectsApi = TestSetupUtil.getAdminAccess().projects();
+    public static final Harvest harvest = TestSetupUtil.getAdminAccess();
+    private static final ProjectsApi projectsApi = harvest.projects();
+    private static final Reference<Client> client = ExistingData.getInstance().getClientReference();
+    private static final Reference<Client> anotherClient = ExistingData.getInstance().getAnotherClientReference();
+    private Project project;
+
+    @AfterEach
+    void afterEach() {
+        if (project != null) {
+            harvest.projects().delete(project);
+        }
+    }
 
     @Test
     void list() {
@@ -39,6 +58,55 @@ class ProjectsApiListTest {
         assertThat(pagination.getPreviousPage()).isNull();
         assertThat(pagination.getPerPage()).isEqualTo(1);
         assertThat(pagination.getTotalPages()).isGreaterThanOrEqualTo(2);
+
+    }
+
+    @Test
+    void listByUpdated() {
+        ProjectFilter filter = new ProjectFilter();
+        filter.setUpdatedSince(Instant.now());
+        List<Project> projects = projectsApi.list(filter);
+
+        assertThat(projects).isEmpty();
+
+    }
+
+    @Test
+    void listByActive(TestInfo testInfo) {
+
+        project = harvest.projects().create(ImmutableProject.builder()
+                .name("Project for " + testInfo.getDisplayName())
+                .billable(true)
+                .billBy(Project.BillingMethod.PROJECT)
+                .budgetBy(Project.BudgetMethod.HOURS_PER_PROJECT)
+                .active(false)
+                .client(ExistingData.getInstance().getClientReference())
+                .build());
+
+        ProjectFilter filter = new ProjectFilter();
+        filter.setActive(false);
+        List<Project> projects = projectsApi.list(filter);
+
+        assertThat(projects).containsExactly(project);
+
+    }
+
+    @Test
+    void listByClient(TestInfo testInfo) {
+
+        project = harvest.projects().create(ImmutableProject.builder()
+                .name("Project for " + testInfo.getDisplayName())
+                .billable(true)
+                .billBy(Project.BillingMethod.PROJECT)
+                .budgetBy(Project.BudgetMethod.HOURS_PER_PROJECT)
+                .client(anotherClient)
+                .build());
+
+        ProjectFilter filter = new ProjectFilter();
+        filter.setClientReference(client);
+
+        List<Project> projects = projectsApi.list(filter);
+        assertThat(projects).doesNotContain(project);
 
     }
 }
