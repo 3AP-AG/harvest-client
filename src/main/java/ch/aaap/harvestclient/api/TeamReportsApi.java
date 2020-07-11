fd3e1b0f3da8ab4012938ade18e20f1db92c93aa
Vladan <vladan@3ap.ch>
2020-07-11 10:41:36 +0200

    PR changes & added BadRequestException

--- src/main/java/ch/aaap/harvestclient/api/TeamReportsApi.java
+++ src/main/java/ch/aaap/harvestclient/api/TeamReportsApi.java
@@ -1,18 +1,15 @@
 package ch.aaap.harvestclient.api;
 
-import ch.aaap.harvestclient.api.filter.TaskFilter;
 import ch.aaap.harvestclient.api.filter.TeamReportFilter;
-import ch.aaap.harvestclient.domain.Task;
 import ch.aaap.harvestclient.domain.TeamReport;
 import ch.aaap.harvestclient.domain.pagination.Pagination;
 import java.util.List;
 
 /**
  * @see <a href=
- *      "https://help.getharvest.com/api-v2/projects-api/projects/projects/">
+ *      "https://help.getharvest.com/api-v2/reports-api/reports/time-reports/#team-report">
  *      Projects API on Harvest</a>
  */
-@Api.Permission(Api.Role.PROJECT_MANAGER)
 public interface TeamReportsApi {
 
     List<TeamReport> list(TeamReportFilter filter);
--- src/test/java/ch/aaap/harvestclient/impl/TeamReportApiImplTest.java
+++ src/test/java/ch/aaap/harvestclient/impl/TeamReportApiImplTest.java
@@ -1,11 +1,14 @@
 package ch.aaap.harvestclient.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
 
 import ch.aaap.harvestclient.HarvestTest;
 import ch.aaap.harvestclient.api.TeamReportsApi;
 import ch.aaap.harvestclient.api.filter.TeamReportFilter;
 import ch.aaap.harvestclient.domain.TeamReport;
+import ch.aaap.harvestclient.exception.BadRequestException;
 import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -20,12 +23,23 @@ class TeamReportApiImplTest {
         TeamReportsApi api = TestSetupUtil.getAdminAccess().teamReports();
 
         TeamReportFilter filter = TeamReportFilter.emptyFilter();
-        filter.setFrom(LocalDate.now().minusYears(1));
-        filter.setTo(LocalDate.now().minusDays(1));
+        filter.setFrom(LocalDate.now().minusYears(1).plusDays(1));
+        filter.setTo(LocalDate.now());
         List<TeamReport> teamReports = api.list(filter);
 
         assertThat(teamReports).isNotNull();
         assertThat(teamReports.size()).isGreaterThan(0);
 
+    }
+    @Test
+    void testReportForMoreThanOneYear() {
+        assertThrows(BadRequestException.class, () -> {
+            TeamReportsApi api = TestSetupUtil.getAdminAccess().teamReports();
+
+            TeamReportFilter filter = TeamReportFilter.emptyFilter();
+            filter.setFrom(LocalDate.now().minusYears(1));
+            filter.setTo(LocalDate.now().plusDays(1));
+            given(api.list(filter)).willThrow(new Exception());
+        });
     }
 }
